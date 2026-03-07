f0a93ad212a4dee19c39eb548fcd46124643f2f7
Elsa <elsahu96@gmail.com>
2026-03-07 18:38:30 +0000

    checkpoint changes

--- backend/app/graph/flow.py
+++ backend/app/graph/flow.py
@@ -11,18 +11,21 @@ Nodes:
 TODO: implement each node function and wire into StateGraph.
 """
 
+from langchain_core.runnables import RunnableConfig
 from app.graph.state import NotesRequest, QueryRequest
-from langsmith import traceable
+from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import END, StateGraph, START
 from app.graph.nodes import NodeExtrator, EdgeExtractor, GraphWriter
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 # ─── Graph assembly ───────────────────────────────────────────────────────────
 
 
-@traceable
 class ProcessNotes:
-    @traceable
+
     def __init__(self):
         builder = StateGraph(NotesRequest)
         builder.add_node("node_extractor", NodeExtrator())
@@ -33,17 +36,21 @@ class ProcessNotes:
         builder.add_edge("node_extractor", "edge_extractor")
         builder.add_edge("edge_extractor", "graph_writer")
         builder.add_edge("graph_writer", END)
-        self.graph = builder.compile()
+        checkpointer = MemorySaver()
+        self.graph = builder.compile(checkpointer=checkpointer)
 
-    @traceable
     async def __call__(self, request):
-        initial_state = {
-            "notes": request.notes,
-            "nodes": [],
-            "edges": [],
-            "status": ""
-        }
-        output = await self.graph.ainvoke(initial_state)
+        initial_state = {"notes": request.notes, "nodes": [], "edges": [], "status": ""}
+        config: RunnableConfig = {"configurable": {"thread_id": "1"}}
+
+        output = await self.graph.ainvoke(initial_state, config)
+
+        state = self.graph.get_state(config)
+        state_history = list(self.graph.get_state_history(config))
+        logger.info(f"State: {state}")
+        # print state_history
+        logger.info(f"State history {state_history[0]}")
+
         return output
 
 
